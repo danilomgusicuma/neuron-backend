7f3b133ce7b2b5c938b1cf64d840ca9bf9b989f7
danilo_miranda <danilo.miranda@conpec.com.br>
2020-04-04 13:37:43 -0300

    minor fix

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.use(cors());
 app.get('/consultation', (request,response)=>response.json({status:'ok'}))
 app.post('/consultation', sendEmailController.sendEmail)
 
-app.listen(3333);
+app.listen('https://institutoneuron.herokuapp.com/');
