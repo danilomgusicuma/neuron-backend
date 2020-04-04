711596c671c56b90652b5c71847c46788b941d6c
danilo_miranda <danilo.miranda@conpec.com.br>
2020-04-04 13:17:39 -0300

    test

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const sendEmailController = require('./sendEmail.js')
 
 const app = express();
 
 app.use(express.json());
 
-console.log(">>>>>>>>>",sendEmailController.sendEmail)
-app.get('/consultation', sendEmailController.sendEmail)
+app.use(cors());
+
+app.get('/consultation', (request,response)=>response.json({status:'ok'}))
+app.post('/consultation', sendEmailController.sendEmail)
 
 app.listen(3333);
--- sendEmail.js
+++ sendEmail.js
@@ -2,7 +2,8 @@ var nodemailer = require('nodemailer');
 
 module.exports = {
   sendEmail(request, response){
-  
+    let body = request.body
+
     let transporter = nodemailer.createTransport({
       host: "br262.hostgator.com.br",
       port: 587,
@@ -16,18 +17,19 @@ module.exports = {
     var mailOptions = {
       from: 'consultas@institutoneuron.com.br',
       to: 'consultas@institutoneuron.com.br',
-      subject: 'DEU CERTO!',
-      text: 'That was easy!'
+      subject: 'Nova Consulta (teste)',
+      text: `${body}`
     };
-  
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-        return response.json({status:error})
-      } else {
-        console.log('Email sent: ' + info.response);
-        return response.json({status:'ok'})
-      }
-    });
+    console.log('sending email')
+    return response.json({status:'test'})
+    // transporter.sendMail(mailOptions, function(error, info){
+    //   if (error) {
+    //     console.log(error);
+    //     return response.json({status:error})
+    //   } else {
+    //     console.log('Email sent: ' + info.response);
+    //     return response.json({status:'ok'})
+    //   }
+    // });
   }
 }
